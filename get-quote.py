be53d53b57060c32e7d479cd34b6695388635ed4
Ramesh Arunachalam <ramesh95arun@yahoo.com>
2019-11-26 14:45:05 -0800

    add additional lines to the quote and loop through

--- get-quote.py
+++ get-quote.py
@@ -2,15 +2,22 @@ import random;
 
 def primary():
    print("Keep it logically awesome.")
-
-   f = open("quotes.txt")
+   f=open("quotes.txt", "a+")
+   for i in range(2):
+     f.write("Appended line %d\r\n" % (i+1))
+   f.close()   
+   f = open("quotes.txt","r")
    quotes = f.readlines()
+   for x in quotes:
+     print x,
    f.close()
    
-   last=len(quotes)-1
-   rnd =random.randint(0,last) 
-  
-   print(quotes[rnd])
+   #last=len(quotes)-1
+   #rnd =random.randint(0,last) 
+   #for x in range(rnd): 
+     #print x
+     #print quotes[x],
+
 
 if __name__== "__main__":
   primary()
